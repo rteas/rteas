d3bfe89d2dec5d0e53dd513328684e1513e60686
buddy <richyteas@google.com>
2019-03-01 11:53:59 -0800

    Updates

---
+++ precache-manifest.3bba2dc2fad976c6fc52c4f6e851885d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b11f53050601e16c4d2",
+    "url": "/static/css/main.36550cea.chunk.css"
+  },
+  {
+    "revision": "9b11f53050601e16c4d2",
+    "url": "/static/js/main.9b11f530.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8a212ff0c4aa4487f555",
+    "url": "/static/js/2.8a212ff0.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c4d4d6dc367767f9249bb0f0f7b3be2f",
+    "url": "/index.html"
+  }
+];
